0fc39d14d05dd198401f896ff4c7e2e0ea21c33c
nidhivanjare <rnidhivanjare@gmail.com>
2020-07-20 17:48:52 +0530

    recipe page

--- app/src/main/java/com/aaditya/reciperecommender/MainActivity.kt
+++ app/src/main/java/com/aaditya/reciperecommender/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.aaditya.reciperecommender
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_click_me = findViewById(R.id.btn_click_me) as Button
+        // set on-click listener
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            startActivity(Intent(this@MainActivity, recipe_display::class.java))
+        }
     }
+
+
+
 }
---
+++ app/src/main/java/com/aaditya/reciperecommender/recipe_display.kt
@@ -0,0 +1,11 @@
+package com.aaditya.reciperecommender
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recipe_display : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_display)
+    }
+}
