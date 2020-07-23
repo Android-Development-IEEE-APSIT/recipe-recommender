21e4508c4a2a1f3c418f5cddba3cd89cceaa8a75
nidhivanjare <rnidhivanjare@gmail.com>
2020-07-23 11:28:50 +0530

    addition

--- app/src/main/java/com/aaditya/reciperecommender/pantry.kt
+++ app/src/main/java/com/aaditya/reciperecommender/pantry.kt
@@ -1,18 +1,11 @@
 package com.aaditya.reciperecommender
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
+class pantry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_pantry)
     }
-
-
-
 }
