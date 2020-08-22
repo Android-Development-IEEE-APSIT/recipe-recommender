6463cde672d5f2f24de81f16be9d38f2768331c3
nidhivanjare <rnidhivanjare@gmail.com>
2020-08-22 13:25:33 +0530

    all recipes - layout

--- app/src/main/java/com/aaditya/reciperecommender/MainActivity.kt
+++ app/src/main/java/com/aaditya/reciperecommender/MainActivity.kt
@@ -7,9 +7,22 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_click_me = findViewById(R.id.btn) as Button
+
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            val intent = Intent(this, pantry::class.java)
+            startActivity(intent)
+
+        }
+
 
 
 
