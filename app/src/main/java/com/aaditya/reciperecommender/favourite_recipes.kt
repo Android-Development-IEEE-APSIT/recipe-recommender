85c8c473a21c936ccd4bacac6695704e1f283042
nidhivanjare <rnidhivanjare@gmail.com>
2020-09-10 20:21:06 +0530

    Merge branch 'master' of /Users/nidhivanjare/AndroidStudioProjects/recipe-recommender with conflicts.

---
+++ app/src/main/java/com/aaditya/reciperecommender/favourite_recipes.kt
@@ -0,0 +1,11 @@
+package com.aaditya.reciperecommender
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class favourite_recipes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourite_recipes)
+    }
+}
