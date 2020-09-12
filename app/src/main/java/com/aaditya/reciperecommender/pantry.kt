34d536be8d22e61049c8ee7fe10c6e9bf39cab2d
nidhivanjare <rnidhivanjare@gmail.com>
2020-09-12 20:27:10 +0530

    minor changes

--- app/src/main/java/com/aaditya/reciperecommender/pantry.kt
+++ app/src/main/java/com/aaditya/reciperecommender/pantry.kt
@@ -1,11 +1,31 @@
 package com.aaditya.reciperecommender
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class pantry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantry)
+
+        val search_recipes = findViewById(R.id.search_recipes) as CardView
+
+        search_recipes.setOnClickListener {
+            val intent = Intent(this, recipe_list::class.java)
+            startActivity(intent)
+
+        }
+
+        val see_list = findViewById(R.id.see_list) as CardView
+
+        search_recipes.setOnClickListener {
+            val intent = Intent(this, Basket::class.java)
+            startActivity(intent)
+
+        }
     }
+
 }
