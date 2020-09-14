8c0fb392ee1aa5ec0b6473d34843d2b794fbe0b8
nidhivanjare <rnidhivanjare@gmail.com>
2020-09-14 16:25:34 +0530

    Side Navgation menu Button

--- app/src/main/java/com/aaditya/reciperecommender/LoginActivity.java
+++ app/src/main/java/com/aaditya/reciperecommender/LoginActivity.java
@@ -183,6 +183,8 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this,"Successful",Toast.LENGTH_SHORT).show();
                     FirebaseUser user = auth.getCurrentUser();
                     updateUI(user);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
                 }
                 else {
                     Toast.makeText(LoginActivity.this,"Failed",Toast.LENGTH_SHORT).show();
